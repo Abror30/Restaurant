2f2d15c3cd51356ba8a80d727ced32ce10e59b01
Abror <94949382+Abror30@users.noreply.github.com>
2023-02-20 18:59:56 +0500

    Update EggOrder.cs
    adjusted the EggOrder class

--- restaurant/EggOrder.cs
+++ restaurant/EggOrder.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,36 +8,38 @@ namespace restaurant
 {
     class EggOrder
     {
-        private int _quantity; 
-        private int? _quality;
+        private int Quantity { get; set; }
+        private static int counter = 0;
+
+        int rnd = 0;
         public EggOrder(int quantity)
         {
-            _quantity = quantity;
+            Quantity = quantity;
+            rnd = new Random().Next(1, 101);
+            counter++;
         }
         public int GetQuantity()
      
         {
-            return _quantity;
+            return Quantity;
         }
         public int? GetQuality()
         {
-            if (_quality == null)  // Check if quality has been calculated yet.
+            if (counter % 2 == 0)
             {
-                Random rnd = new Random();
-                // Only calculate quality half of the time.
-                if (rnd.Next(2) == 0)  // 1/2 chance of returning a value.
-                {
-                    _quality = rnd.Next(1, 101);  // Generate random number between 1 and 100.
-                }
+                return null;
             }
-            return _quality;
+            if (rnd < 25)
+            {
+                return null;
+            }
+            else
+            {
+                return rnd;
+            }
         }
-        public void Crack()  // Throws exception on egg quality < 25.
+        public void Crack()  
         {
-            if (GetQuality() < 25)  // Check for rotten egg.
-            {
-                throw new Exception("Rotten egg!");  // Throw exception on bad egg.
-            }
             Console.WriteLine("the employee cracks the eggs");  // Code to crack eggs here... 
         }
         public void DiscardShell()  // No arguments or return value.
